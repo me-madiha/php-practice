056716a362c60a377ffbc12c265980703ca15a25
Madiha Iqbal <mohdiqbal.madiha333@gmail.com>
2024-11-22 05:24:44 +0500

    Add files via upload

---
+++ practice1.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+////Class1 practice/////
+/*echo("I am madiha");
+echo("<h1>I am learning php</h1>");
+$name = "javascript";
+echo("Recently , I learned"." " .$name);
+///
+$num1 =10;
+$num2 = 5;
+echo($num1>$num2);*/
+
+/////Class2 practice////
+/*$num1 = 20;
+$num2 = 10;
+echo($num1+$num2);
+echo($num1-$num2);
+echo($num1*$num2);
+echo($num1/$num2);*/
+
+/*$num =10 ;
+$num =40 ;
+echo($num);*/
+
+// $num1 = 10;
+// $num2 = 5;
+// echo ($num1 != $num2);
+// echo ($num1 === $num2);
+// echo ($num1 >= $num2);
+//  echo ($num1 !== $num2);
+
+
+$admin = "Madiha";
+$pass = 123;
+$admin2 = "Warisha";
+$pass2 = 987;
+
+// if ($admin == "Madiha" && $pass == 123 || $admin2 == "Warisha" && $pass2 == 987) {
+//     echo ("Welcome Admin");
+// } else {
+//     echo ("Login Filed");
+// }
+
+// switch($admin == "Madiha" && $pass == 1234 || $admin2 == "Warisha" && $pass2 == 9887){
+//     case true:
+//         echo("login successfull");
+//         break;
+//         default:
+//         echo("Login Denied");
+//          break;
+// }
+
+// for ($i = 0; $i < 5; $i++) {
+//         if ($i == 5) {
+//             continue;
+//         }
+//         echo ("<h1>This is For Loop</h1>".$i ." <br>");
+//     }
+
+
+
+?>
